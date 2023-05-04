f2add2a2e0851ed703ef56ddf6fc14aa7107a27c
Benjamin Sale <salebs@outlook.com>
2023-05-04 10:03:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,74 @@
-import numpy
+import math
+from itertools import groupby
 
 
-def find_path(lst, a, m, outs):
-    lst_0 = []
-    for l in lst:
-        if l + a > max(outs) or l == -1:
-            lst_0.append(-1)
-        else:
-            lst_0.append(l + a)
-        if l * m > max(outs) or l == -1 or l * m == l:
-            lst_0.append(-1)
-        else:
-            lst_0.append(l * m)
-    return lst_0
+class Node(object):
+    def __init__(self, value, weight, n, steps):
+        self.value = value
+        self.weight = weight
+        self.n = n
+        self.steps = steps
+        self.dif_val = weight - value
+        self.add_rule = False
 
 
-def decimal_binary(n, s):
-    b = "{0:b}".format(int(n))
-    while len(b) < s:
-        b = '0' + b
-    return b
+def lt(val):
+    return val[1] * len(val[2])
 
 
-def aCm(a, m, i_l, i_h, o_l, o_h):
-    inputs = list(range(i_l, i_h + 1))
-    outputs = list(range(o_l, o_h + 1))
+def small(lst):
+    return len(lst)
+
+
+def acm(a, m, i_l, i_h, o_l, o_h):
+    inputs, outputs = list(range(i_l, i_h + 1)), list(range(o_l, o_h + 1))
+    # check for base cases impossible
     if len(inputs) > len(outputs):
         return 'impossible'
-    # check if inputs are already in output
-    check_0 = [inp for inp in inputs if inp in outputs]
-    if len(check_0) == len(inputs):
-        return 'empty'
-    # check if inputs are greater than outputs (impossible to reduce)
     if [inp for inp in inputs if inp > max(outputs)]:
         return 'impossible'
-    # find fastest path to output range
-    lst_inp = [inputs[0]]
-    operation, method = False, ''
-    while not operation:
-        while max(lst_inp) not in outputs:
-            lst_inp = find_path(lst_inp, a, m, outputs)
-            if lst_inp == [-1] * len(lst_inp):
-                return 'impossible'
-        lst_ind = [lst_inp.index(inp) for inp in lst_inp if inp in outputs]
-        step = numpy.log(len(lst_inp)) / numpy.log(2)
-        lst_bin = [decimal_binary(ind, step) for ind in lst_ind]
-        for str_bin in lst_bin:
-            operation = True
-            for inp in inputs[1:]:
-                tmp_inp = inp
-                for op in str_bin:
-                    if op == '0':
-                        tmp_inp += a
-                    else:
-                        tmp_inp = tmp_inp * m
-                if tmp_inp not in outputs:
-                    operation = False
-                    break
-            if operation:
-                method = str_bin
-                break
+    # check for base case empty
+    if len([inp for inp in inputs if inp in outputs]) == len(inputs):
+        return 'empty'
+    # find path to output range
+    max_n, paths = math.floor(math.log((o_h - o_l) / (i_h - i_l), m)), []
+    node = Node(i_l, o_h - (i_h - i_l) * (m ** 0), 0, '')
+    nodes, cur_node, rule = [[node, node.dif_val, node.steps]], node, [0]
+    while rule:
+        if paths:
+            rule = [n for n in nodes if len(n[2]) < min([len(path) for path in paths])]
+        nodes = [n for n in nodes if n[0].steps != '']
+        add_node = Node(cur_node.value + a, cur_node.weight, cur_node.n, cur_node.steps + '0')
+        if o_l <= cur_node.value <= cur_node.weight:
+            paths.append(cur_node.steps)
+        else:
+            if not cur_node.add_rule:
+                if add_node.dif_val >= 0:
+                    nodes.append([add_node, add_node.dif_val, add_node.steps])
+                if cur_node.n < max_n:
+                    mul_node = Node(cur_node.value * m, o_h - (i_h - i_l) * (m ** (cur_node.n + 1)), cur_node.n + 1,
+                                    cur_node.steps + '1')
+                    if mul_node.dif_val < 0:
+                        add_node.add_rule = True
+                    elif mul_node.n == max_n:
+                        mul_node.add_rule = True
+                    if mul_node.n <= max_n and mul_node.dif_val >= 0:
+                        nodes.append([mul_node, mul_node.dif_val, mul_node.steps])
             else:
-                lst_inp = find_path(lst_inp, a, m, outputs)
-    if len(method) > 1:
-        path, prev_ele = method[0], method[0]
-        for next_ele in method[1:]:
-            if prev_ele == next_ele:
-                path += next_ele
-            else:
-                path += ',' + next_ele
-                prev_ele = next_ele
-        path = path.split(',')
-    else:
-        path = [method]
-    final_path = ' '.join([str(len(x)) + 'M' if '1' in x else str(len(x)) + 'A' for x in path])
-    return final_path
+                if cur_node.value + math.ceil((o_l - cur_node.value) / a) * a <= cur_node.weight:
+                    paths.append(cur_node.steps + f'{"0" * math.ceil((o_l - cur_node.value) / a)}')
+        if nodes:
+            nodes.sort(key=lt)
+            cur_node = nodes.pop(0)[0]
+        elif not nodes and not paths:
+            return 'impossible'
+    if not paths:
+        return 'impossible'
+    if o_l <= cur_node.value <= cur_node.weight:
+        paths.append(cur_node.steps)
+    paths.sort(key=small)
+    return ' '.join([str(len(x)) + 'M' if '1' in x else str(len(x)) + 'A' for x in
+                     [''.join(g) for _, g in groupby(paths[0])]])
 
 
 f = open("input.txt", 'r')
@@ -84,12 +78,12 @@ input_lst = []
 for line in lines:
     if line != '0 0 0 0 0 0':
         line = line[:-1].split(" ")
-        input_lst.append([int(l) for l in line])
+        input_lst.append([int(li) for li in line])
 output_lst = []
-for inp in input_lst:
-    adding, multiplier, input_low, input_high, output_low, output_high = inp
-    output = aCm(adding, multiplier, input_low, input_high, output_low, output_high)
-    print(inp, output)
+for i in input_lst:
+    adding, multiplier, input_low, input_high, output_low, output_high = i
+    output = acm(adding, multiplier, input_low, input_high, output_low, output_high)
+    print(i, output)
     output_lst.append(output + '\n')
 f0 = open("output.txt", 'w')
 f0.writelines(output_lst)
